9af041d3f6adb778fa5e1f2ee98e9bf007420ad1
kaneeshk24 <kaneeshk24>
2024-05-18 10:54:08 +0000

    Pushed from Locofy Builder

---
+++ pages/popup.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "./popup.module.css";
+
+const Popup: NextPage = () => {
+  return <div className={styles.popup} />;
+};
+
+export default Popup;
